15c59ac2ac0b2d903e9b0f8fda01511f434ce057
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2020-05-29 15:46:53 -0400

    add saga method call and comments

--- use-redux-saga-for-async-calls/src/saga/saga.js
+++ use-redux-saga-for-async-calls/src/saga/saga.js
@@ -3,15 +3,29 @@ takeLatest,
 takeEvery,
 // eslint-disable-next-line
 throttle,
-  delay,
+  delay, 
   put,
+  call
 } from 'redux-saga/effects';
 
 function* ageUpAsync() {
   yield put({type: 'LOADING'})
-  yield delay(3000);
+  // yield delay(3000);
+  call(delay, 3000)
+  // type should be always different not to make infinite loop
   yield put({ type: 'AGE_UP_ASYNC', value: 1 });
 }
+// const delay = (ms) => new Promise(res => setTimeout(res, ms))
+// Instead of doing yield delay(3000), we're now doing yield call(delay, 3000). What's the difference?
+// In the first case, the yield expression delay(1000) is evaluated before it gets passed to the caller of next (the caller could be the middleware when running our code. It could also be our test code which runs the Generator function and iterates over the returned Generator). So what the caller gets is a Promise, like in the test code above.
+
+// In the second case, the yield expression call(delay, 1000) is what gets passed to the caller of next. call just like put, returns an Effect which instructs the middleware to call a given function with the given arguments. In fact, neither put nor call performs any dispatch or asynchronous call by themselves, they return plain JavaScript objects.
+
+// put({type: 'INCREMENT'}) // => { PUT: {type: 'INCREMENT'} }
+// call(delay, 1000)        // => { CALL: {fn: delay, args: [1000]}}
+// What happens is that the middleware examines the type of each yielded Effect then decides how to fulfill that Effect. If the Effect type is a PUT then it will dispatch an action to the Store. If the Effect is a CALL then it'll call the given function.
+
+// This separation between Effect creation and Effect execution makes it possible to test our Generator in a surprisingly easy way:
 
 export function* watchAgeUp() {
   // call to get all clicks event
@@ -26,13 +40,13 @@ export function* watchAgeUp() {
 }
 
 
-// saga/effects method takeEvery() only all clicks
+// saga/effects method takeEvery() gets all clicks
 // eslint-disable-next-line
 function* takeEveryClick() {
   yield takeEvery('AGE_UP', ageUpAsync);
 }
 
-// saga/effects method takeLatest() only gets latest one click
+// saga/effects method takeLatest() gets only latest, one click
 function* takeLatestClick() {
   yield takeLatest('AGE_UP', ageUpAsync);
 }
